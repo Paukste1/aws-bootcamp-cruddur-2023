9ac8a161d538e32eb5fb3acd660dbad7a436c179
Paukste1 Paukštė <vlukasp@gmail.com>
2024-01-21 13:05:04 +0000

    minor fixes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,7 +21,7 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
+  expose_headers=["location,link"],
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
